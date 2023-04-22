fa2b9a34d55fe454c46ab12ec9fe9422af87034a
Lien <ayalp.ende@gmail.com>
2023-04-22 10:07:24 +0200

    Initial Commit

---
+++ src/test/java/com/asmcm/moderation/ModerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asmcm.moderation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
